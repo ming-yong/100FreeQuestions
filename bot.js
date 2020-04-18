a7f70e7b757b7c8f198a7062ac293bf8ee2dff1a
mingyong.dev <mingyong.dev@gmail.com>
2020-04-18 01:00:01 -0400

    change hashtag

--- bot.js
+++ bot.js
@@ -3,20 +3,9 @@ var Twit = require('twit');
 var config = require("./config");
 var T = new Twit(config);
 
-var stream1 = T.stream('statuses/filter', { track: '#commentMing'});
-var stream2 = T.stream('statuses/filter', { track: '#proQuestions'});
+var stream1 = T.stream('statuses/filter', { track: '#🤔'});
 
-stream1.on('tweet', function (tweet) {
-  T.post('statuses/retweet/:id', { id: tweet.id_str }, function (err, data, response) {
-    if(err){
-      console.err(err);
-    }else{
-      console.log("retweet is working!");
-    }
-  })
-})
-
-stream2.on('tweet', function (tweet) {
+stream.on('tweet', function (tweet) {
   T.post('statuses/retweet/:id', { id: tweet.id_str }, function (err, data, response) {
     if(err){
       console.err(err);
